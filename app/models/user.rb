6e4c9fe5ec2863249c6f24b25d179981f3b3c1a8
abc <arslan@fliclicks.comgit config --global user.name arslansaeed0034>
2019-08-27 07:51:46 -0400

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-	
+	validates :username,presence: true,
+	uniqueness:{ case_senstive: false },
+	 length: { minimum:4, maximum:12 } 
+
+	 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	 validates :email,presence: true,
+	 uniqueness:{ case_senstive: false },
+	 format: { with: VALID_EMAIL_REGEX }
+
 end
